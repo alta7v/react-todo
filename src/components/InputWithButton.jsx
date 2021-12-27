f1465ae17cfbb13723de842e8983f0a7b45f3903
alta7v <aquila.alta7v@gmail.com>
2021-12-27 20:32:21 +0900

    sample CSS in JS

--- src/components/InputWithButton.jsx
+++ src/components/InputWithButton.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
+const style = {
+  backgroundColor: "#c1ffff",
+  width: "400px",
+  height: "30px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "8px"
+};
+
 export const InputWithButton = (props) => {
   const { placeholder, text, onChange, onClick } = props;
   return (
-    <div className="input-area">
+    <div style={style}>
       <input placeholder={placeholder} value={text} onChange={onChange} />
       <button onClick={onClick}>追加</button>
     </div>
